62823119721b5951c06c638cb64721ff8f7d66e5
jackyshan <jackyshan@email.com>
2016-10-10 18:03:23 +0800

    实现搜索商品列表

---
+++ pages/productList/productList.js
@@ -0,0 +1,109 @@
+Page({
+  data:{
+    // text:"这是一个页面"
+    hidden: false,
+    //搜索名称
+    searchName: null,
+    //pageindex
+    pageIdx: 1,
+    //产品列表
+    productArr:[]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    console.log(options)
+
+    var that = this
+
+    var searchName = options.searchName
+    console.log(searchName)
+    
+    that.setData({
+        searchName:searchName
+    })
+
+    var params = {}
+    params["page_index"] = 1
+    params["page_size"] = 10
+    params["keyword"] = searchName
+    
+    console.log(JSON.stringify(params))
+
+    wx.request({
+      url: 'http://192.168.0.10:8090/v2.5/product/search',
+      method: 'GET',
+      data: {
+        search_param: JSON.stringify(params),
+        y: ''
+      },
+      header: {
+          'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res.data)
+
+        that.setData({
+            hidden: true
+        })
+
+        console.log(Array.from(res.data.data.products))
+        that.setData({
+          productArr:Array.from(res.data.data.products)
+        })
+      }
+    })
+  },
+  onReady:function(){
+    // 页面渲染完成
+    var that = this
+    wx.setNavigationBarTitle({
+        title:that.data.searchName
+    })
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  },
+
+  //上拉更多
+  lower:function(){
+    var that = this;
+    console.log("shanglale")
+    var params = {}
+    params["page_index"] = ++that.data.pageIdx
+    params["page_size"] = 10
+    params["keyword"] = that.data.searchName
+
+    console.log(JSON.stringify(params))
+
+    wx.request({
+    url: 'http://192.168.0.10:8090/v2.5/product/search',
+    method: 'GET',
+    data: {
+        search_param: JSON.stringify(params),
+        y: ''
+    },
+    header: {
+        'Content-Type': 'application/json'
+    },
+    success: function(res) {
+        console.log(res.data)
+
+        that.setData({
+            hidden: true
+        })
+
+        console.log(Array.from(res.data.data.products))
+        that.setData({
+        productArr:that.data.productArr.concat(Array.from(res.data.data.products))
+        })
+    }
+    })
+  },
+
+})
--- pages/searchs/searchs.js
+++ pages/searchs/searchs.js
@@ -4,7 +4,8 @@ var app = getApp()
 Page({
   data: {
     motto: '阳淘',
-    userInfo: {}
+    userInfo: {},
+    rcmdArr: []
   },
   //事件处理函数
   bindViewTap: function() {
@@ -26,5 +27,31 @@ Page({
   bindKeyInput: function(e) {
     var value = e.detail.value
     console.log(value)
+
+    var that = this
+    wx.request({
+      url: 'http://192.168.0.10:8090/v2.2/app/searchkey',
+      method: 'GET',
+      data: {
+        key: value,
+        y: ''
+      },
+      header: {
+          'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        console.log(Array.from(res.data.data))
+        that.setData({
+          rcmdArr:Array.from(res.data.data)
+        })
+      }
+    })
+    
   },
+  goToPList: function(e) {
+    console.log(e.currentTarget.id)
+    wx.navigateTo ({
+      url: '../productList/productList?searchName='+e.currentTarget.id
+    })
+  }
 })
